7f210bb5d61f65f0e7ad16f030e0371055e7f9dd
PolCarva <pablocarvalhogimenez@gmail.com>
2024-04-11 11:43:39 -0300

    Responsive project detail page.

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -12,54 +12,90 @@ const ProjectDetail = () => {
     <MainLayout>
       <section className="w-full h-full py-6 px-6 container mx-auto gap-1 ">
         {project && (
-          <div>
-            <ul className="flex justify-between text-lg md:grid grid-cols-12 md:grid-cols-10 gap-6 mb-6">
-              <li className="col-span-3">
-                {project.index > 9 ? project.index : `0${project.index}`}
-              </li>
-              <li className="col-span-4 flex w-full justify-between">
-                <span>{project.name}</span>
-                <span>{project.year}</span>
-              </li>
-              <li className="col-span-3 text-right">{project.project}</li>
-            </ul>
-            <ul className="flex justify-between md:grid grid-cols-12 md:grid-cols-10 gap-6 text-sm text-gray-500 mb-6">
-              <li className="col-span-3 grid grid-cols-3 italic text-gray-400 text-justify">
-                <span className="col-span-2">{project.description}</span>
-              </li>
-              <li className="col-span-7 ">
-                <div className="min-h-[50svh] flex flex-col gap-5">
-                  {project.files.map((file, index) => {
-                    return project.category.toLowerCase() === "film" ? (
-                      <div>
+          <>
+            {/* Desktop */}
+            <div className="hidden md:block">
+              <ul className="flex justify-between text-lg md:grid grid-cols-12 md:grid-cols-10 gap-6 mb-6">
+                <li className="col-span-3">
+                  {project.index > 9 ? project.index : `0${project.index}`}
+                </li>
+                <li className="col-span-4 flex w-full justify-between">
+                  <span>{project.name}</span>
+                  <span>{project.year}</span>
+                </li>
+                <li className="col-span-3 text-right">{project.project}</li>
+              </ul>
+              <ul className="flex justify-between md:grid grid-cols-12 md:grid-cols-10 gap-6 text-sm text-gray-500 mb-6">
+                <li className="col-span-3 grid grid-cols-3 italic text-gray-400 text-justify">
+                  <span className="col-span-2">{project.description}</span>
+                </li>
+                <li className="col-span-7 ">
+                  <div className="min-h-[50svh] flex flex-col gap-5">
+                    {project.files.map((file, index) => {
+                      return project.category.toLowerCase() === "film" ? (
+                        <div>
+                          <div className="grid grid-cols-7">
+                            <Vimeo
+                              key={file}
+                              video={file}
+                              className="aspect-video col-span-4"
+                            />
+                            <span className="col-span-3 text-right">
+                              {index + 1 > 9 ? index + 1 : `0${index + 1}`}
+                            </span>
+                          </div>
+                          <p className="whitespace-pre-line mt-10 text-base">
+                            {project.body}
+                          </p>
+                        </div>
+                      ) : (
                         <div className="grid grid-cols-7">
-                          <Vimeo
-                          
-                            key={file}
-                            video={file}
-                            className="aspect-video col-span-4"
+                          <img
+                            src={`../src/assets/img/${file}.jpg`}
+                            alt={file}
+                            className="col-span-4"
                           />
                           <span className="col-span-3 text-right">
                             {index + 1 > 9 ? index + 1 : `0${index + 1}`}
                           </span>
                         </div>
-                        <p className="whitespace-pre-line mt-10 text-base">
-                          {project.body}
-                        </p>
-                      </div>
-                    ) : (
-                      <div className="grid grid-cols-7">
-                        <img src={`../src/assets/img/${file}.jpg`} alt={file} className="col-span-4" />
-                        <span className="col-span-3 text-right">
-                          {index + 1 > 9 ? index + 1 : `0${index + 1}`}
-                        </span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </li>
-            </ul>
-          </div>
+                      );
+                    })}
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+            {/* Mobile */}
+            <div className="flex flex-col gap-5 md:hidden">
+              <h2 className="text-xl">{project.name}</h2>
+              {project.files.map((file, index) => {
+                return project.category.toLowerCase() === "film" ? (
+                  <div>
+                    <Vimeo
+                      key={file}
+                      video={file}
+                      className="aspect-video w-full"
+                    />
+                    <p className="whitespace-pre-line mt-10 text-base">
+                      {project.body}
+                    </p>
+                  </div>
+                ) : (
+                  <div className="relative">
+                    <img
+                      src={`../src/assets/img/${file}.jpg`}
+                      alt={file}
+                      className="col-span-4"
+                    />
+                    <span className="absolute top-2 right-2 text-gray-400">
+                      {index + 1 > 9 ? index + 1 : `0${index + 1}`}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          </>
         )}
         {!project && (
           <p className="text-center h-[60svh] grid place-content-center text-gray-400">
