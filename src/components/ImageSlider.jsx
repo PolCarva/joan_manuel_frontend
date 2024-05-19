318a9c0d67e3cb863c0aa66c7b9430fdf2c61e22
PolCarva <pablocarvalhogimenez@gmail.com>
2024-05-19 18:09:02 -0300

    Add escape key to Image Slider

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { stables } from "../constants/stables";
 import { Keyboard } from "swiper/modules";
 
 const ImageSlider = ({ images, index, close }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    if (modalRef.current) {
+      modalRef.current.focus();
+    }
+  }, []);
 
   return (
     <div
       className="w-full h-full bg-black/60 fixed z-50 inset-0"
+      ref={modalRef}
+      tabIndex={-1}
+      onKeyDown={(e) => {
+        if (e.key === "Escape") {
+          close();
+        }
+      }}
       onClick={() => {
         close();
       }}
--- src/constants/stables.jsx
+++ src/constants/stables.jsx
@@ -1,6 +1,5 @@
-const isDev = process.env.NODE_ENV === 'development';
-const BASE_URL = isDev ? 'http://localhost:3000/api' : 'https://joanmanuel.up.railway.app/api';
-const MEDIA_URL = isDev ? 'http://localhost:3000/media' : 'https://joanmanuel.up.railway.app/media';
+const BASE_URL = 'https://joanmanuel.up.railway.app/api';
+const MEDIA_URL = 'https://joanmanuel.up.railway.app/media';
 
 export const stables = {
     BASE_URL,
